c71a1f4761be5daca7fd22058dca2a49f6fd288d
Jason Zhang <jz@jzfed.com>
2020-06-27 01:32:04 +0800

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('datatable-i18n-angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('datatable-i18n-angular app is running!');
-  });
 });
